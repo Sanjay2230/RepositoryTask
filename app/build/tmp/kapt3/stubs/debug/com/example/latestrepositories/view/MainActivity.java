fc35ca5d87753d8712e0d0da021f7e29328b7225
sanjay1 <nilaa0508>
2021-11-08 14:47:51 +0530

    Finished the refreshing the layout and changed appbar and status bar color.

--- app/build/tmp/kapt3/stubs/debug/com/example/latestrepositories/view/MainActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/latestrepositories/view/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.latestrepositories.view;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u000b\u001a\u00020\fJ\u0012\u0010\r\u001a\u00020\f2\b\u0010\u000e\u001a\u0004\u0018\u00010\u000fH\u0014R\u001e\u0010\u0003\u001a\u0012\u0012\u0004\u0012\u00020\u00050\u0004j\b\u0012\u0004\u0012\u00020\u0005`\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0010"}, d2 = {"Lcom/example/latestrepositories/view/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "repoList", "Ljava/util/ArrayList;", "Lcom/example/latestrepositories/model/entity/Repositories;", "Lkotlin/collections/ArrayList;", "repoListAdapter", "Lcom/example/latestrepositories/view/adapters/RepoListAdapter;", "repositoryViewModel", "Lcom/example/latestrepositories/viewmodel/RepositoryViewModel;", "networkCall", "", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
-public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\u0006\u0010\f\u001a\u00020\rJ\u0006\u0010\u000e\u001a\u00020\rJ\u0012\u0010\u000f\u001a\u00020\r2\b\u0010\u0010\u001a\u0004\u0018\u00010\u0011H\u0014J\b\u0010\u0012\u001a\u00020\rH\u0016J\u0006\u0010\u0013\u001a\u00020\rR\u001e\u0010\u0004\u001a\u0012\u0012\u0004\u0012\u00020\u00060\u0005j\b\u0012\u0004\u0012\u00020\u0006`\u0007X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\tX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\n\u001a\u00020\u000bX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0014"}, d2 = {"Lcom/example/latestrepositories/view/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "Landroidx/swiperefreshlayout/widget/SwipeRefreshLayout$OnRefreshListener;", "()V", "repoList", "Ljava/util/ArrayList;", "Lcom/example/latestrepositories/model/entity/Repositories;", "Lkotlin/collections/ArrayList;", "repoListAdapter", "Lcom/example/latestrepositories/view/adapters/RepoListAdapter;", "repositoryViewModel", "Lcom/example/latestrepositories/viewmodel/RepositoryViewModel;", "networkCall", "", "objectInitialization", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onRefresh", "showRepoList", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity implements androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener {
     private com.example.latestrepositories.viewmodel.RepositoryViewModel repositoryViewModel;
     private com.example.latestrepositories.view.adapters.RepoListAdapter repoListAdapter;
     private java.util.ArrayList<com.example.latestrepositories.model.entity.Repositories> repoList;
@@ -18,6 +18,16 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
     android.os.Bundle savedInstanceState) {
     }
     
+    public final void objectInitialization() {
+    }
+    
+    public final void showRepoList() {
+    }
+    
     public final void networkCall() {
+    }
+    
+    @java.lang.Override()
+    public void onRefresh() {
     }
 }
--- app/src/main/java/com/example/latestrepositories/view/MainActivity.kt
+++ app/src/main/java/com/example/latestrepositories/view/MainActivity.kt
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.latestrepositories.R
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModelProviders.of
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.latestrepositories.model.entity.Repositories
 import com.example.latestrepositories.networks.ApiInterface
 import com.example.latestrepositories.view.adapters.RepoListAdapter
 import com.example.latestrepositories.viewmodel.RepositoryViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var repositoryViewModel: RepositoryViewModel
     private lateinit var repoListAdapter: RepoListAdapter
@@ -34,15 +37,20 @@ class MainActivity : AppCompatActivity() {
     fun objectInitialization(){
         repositoryViewModel = of(this)[RepositoryViewModel::class.java]
         repoListAdapter = RepoListAdapter(this)
+        refreshLayout.setOnRefreshListener(this)
+        refreshLayout.setColorSchemeResources(android.R.color.white)
+        refreshLayout.setProgressBackgroundColorSchemeResource(R.color.blue_900)
     }
 
     fun showRepoList(){
+        progress.visibility = View.VISIBLE
         repo_list.adapter = repoListAdapter
         repo_list.layoutManager = LinearLayoutManager(this)
         repo_list.addItemDecoration(DividerItemDecoration(this, VERTICAL))
         repoList = ArrayList()
         repositoryViewModel.getRepoList {
             it.observe(this, { v ->
+                progress.visibility = View.GONE
                 repoList = v as ArrayList<Repositories>
                 repoListAdapter.setData(repoList)
             })
@@ -54,6 +62,7 @@ class MainActivity : AppCompatActivity() {
         apiInterface.enqueue(object : Callback<List<Repositories>> {
             override fun onResponse(call: Call<List<Repositories>>, response: Response<List<Repositories>>) {
                 if (response.isSuccessful) {
+                    refreshLayout.isRefreshing = false
                     Log.d("Response", "OnScuccess: " + response.message())
                     Log.d("Response", "OnScuccess: " + response.body().toString())
                     repositoryViewModel.insert(response.body()!!)
@@ -64,5 +73,9 @@ class MainActivity : AppCompatActivity() {
                 Log.d("main", "onFailure: " + t.message)
             }
         })
+    }
+
+    override fun onRefresh() {
+        networkCall()
     }
 }
