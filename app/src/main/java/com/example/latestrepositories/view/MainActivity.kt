052eab0a367792686c49b4c6841f6529ca6a6b45
sanjay1 <nilaa0508>
2021-11-16 12:54:33 +0530

    Fixed the minor issues.

--- app/src/main/java/com/example/latestrepositories/view/MainActivity.kt
+++ app/src/main/java/com/example/latestrepositories/view/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.example.latestrepositories.view
 
 import android.annotation.SuppressLint
-import android.app.SearchManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import com.example.latestrepositories.R
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.SearchView
-import android.widget.SearchView.OnQueryTextListener;
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
+import androidx.appcompat.widget.SearchView
+import androidx.annotation.RequiresApi
+import androidx.core.view.MenuItemCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProviders.of
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.latestrepositories.R
 import com.example.latestrepositories.model.entity.Repositories
 import com.example.latestrepositories.networks.ApiInterface
+import com.example.latestrepositories.utils.Utils
+import com.example.latestrepositories.utils.goneView
+import com.example.latestrepositories.utils.showView
 import com.example.latestrepositories.view.adapters.RepoListAdapter
 import com.example.latestrepositories.viewmodel.RepositoryViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,22 +34,6 @@ import kotlinx.android.synthetic.main.content_activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import android.content.Context
-import android.os.Build
-import android.text.InputType
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.core.view.MenuItemCompat
-import androidx.core.widget.doAfterTextChanged
-import androidx.recyclerview.widget.RecyclerView
-import com.example.latestrepositories.utils.RecyclerViewPagination
-import com.example.latestrepositories.utils.Utils
-import com.example.latestrepositories.utils.goneView
-import com.example.latestrepositories.utils.showView
-import org.json.JSONArray
-import org.json.JSONObject
 
 
 class MainActivity : Utils(), SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
@@ -54,15 +47,18 @@ class MainActivity : Utils(), SwipeRefreshLayout.OnRefreshListener, View.OnClick
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
         objectInitialization()
         showRepoList()
         if(isNetworkAvailable()){
             networkCall()
         }else{
-            error.showView()
-            retry.showView()
-            repo_list.goneView()
+            if(currentInt == 0){
+                progress.goneView()
+                error.showView()
+                retry.showView()
+                repo_list.goneView()
+            }
         }
     }
 
@@ -148,6 +144,7 @@ class MainActivity : Utils(), SwipeRefreshLayout.OnRefreshListener, View.OnClick
             }
             networkCall()
         }else{
+            error.text = getString(R.string.netword_issue)
             refreshLayout.isRefreshing = false
             error.showView()
             retry.showView()
@@ -178,16 +175,16 @@ class MainActivity : Utils(), SwipeRefreshLayout.OnRefreshListener, View.OnClick
 
         val menuItem: MenuItem? = menu?.findItem(R.id.search_id)
 
-        val searchView: androidx.appcompat.widget.SearchView? = MenuItemCompat.getActionView(menuItem) as androidx.appcompat.widget.SearchView?
-        val imageView: ImageView? = searchView?.findViewById(androidx.appcompat.R.id.search_button)
+        val searchView: androidx.appcompat.widget.SearchView = MenuItemCompat.getActionView(menuItem) as androidx.appcompat.widget.SearchView
+        val imageView: ImageView? = searchView.findViewById(androidx.appcompat.R.id.search_button)
         val cancelImageView: ImageView? =
-            searchView?.findViewById(androidx.appcompat.R.id.search_close_btn)
+            searchView.findViewById(androidx.appcompat.R.id.search_close_btn)
 
         imageView?.setImageDrawable(getDrawable(R.drawable.ic_baseline_search_24))
         cancelImageView?.setImageDrawable(getDrawable(R.drawable.ic_baseline_clear_24))
 
         val editText =
-            (searchView?.findViewById(androidx.appcompat.R.id.search_src_text) as EditText?)!!
+            (searchView.findViewById(androidx.appcompat.R.id.search_src_text) as EditText?)!!
 
         editText.setTextColor(resources.getColor(R.color.black))
         editText.setHintTextColor(resources.getColor(R.color.black))
@@ -204,35 +201,32 @@ class MainActivity : Utils(), SwipeRefreshLayout.OnRefreshListener, View.OnClick
         } catch (e: Exception) {
         }
 
-        menuItem?.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-                return true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
             }
 
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-                repoListAdapter.setData(repoList)
-                return true
-            }
-        })
-
-        editText.doAfterTextChanged {
-
-            if (!it.isNullOrBlank()) {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (!editText.text.toString().isNullOrBlank()) {
 //                repoListAdapter.setData(repoList)
-                repoListAdapter.filter.filter(it.toString())
-            } else {
-                repoListAdapter.setData(repoList)
+                    repoListAdapter.filter.filter(editText.text.toString())
+                } else {
+                    repoListAdapter.setData(repoList)
+                }
+                return false
             }
-        }
+
+        })
     }
 
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.retry -> {
                 if(isNetworkAvailable()){
+                    error.text = getString(R.string.loading)
                     networkCall()
                 }else{
+                    error.text = getString(R.string.netword_issue)
                     error.showView()
                     retry.showView()
                     repo_list.goneView()
